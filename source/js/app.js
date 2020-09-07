bb686db8d3128421a61cf50d31b4b9cf987b9b8d
mAnic <arctic.manic@gmail.com>
2020-09-07 09:41:18 +0300

    markup

--- source/js/app.js
+++ source/js/app.js
@@ -1,20 +1,18 @@
 "use strict";
 
-let toggleMenuOpen = document.querySelector(".page-header__toggle--open");
-let toggleMenuClose = document.querySelector(".page-header__toggle--close");
+// Header Menu Toggler
+
+let toggleMenu = document.querySelector(".menu-toggler");
 let headerMenu = document.querySelector(".page-header__menu");
 
-toggleMenuOpen.addEventListener("click", function(event) {
+toggleMenu.addEventListener("click", function (event) {
   event.preventDefault();
 
-  headerMenu.classList.remove("page-header__menu--closed");
+  headerMenu.classList.toggle("page-header__menu--closed");
+  this.classList.toggle("menu-toggler--open");
 });
 
-toggleMenuClose.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  headerMenu.classList.add("page-header__menu--closed");
-});
+// Add Select
 
 let selectDataClose = document.querySelectorAll(".custom-select__data-close");
 let selectDataOpen = document.querySelectorAll(".add-plan__field--select .custom-select__label");
@@ -54,4 +52,22 @@ customSelectAdd.forEach(element => {
 
     console.log(newSelect)
   });
+});
+
+// Tariff Block Toggler
+
+let toggleTariffsShow = document.querySelector(".add-profile__show");
+let toggleTariffsHide = document.querySelector(".tariffs__close");
+let toggleBlock = document.querySelector(".tariffs");
+
+toggleTariffsShow.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  toggleBlock.classList.add("tariffs--show");
+})
+
+toggleTariffsHide.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  toggleBlock.classList.remove("tariffs--show");
 });
